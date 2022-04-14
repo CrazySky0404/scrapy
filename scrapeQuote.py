a65eb34d874dc6dd63d67c8f4c60b7b0660968be
MaritaBaro <crazysky700@gmail.com>
2022-04-14 14:30:19 +0300

    One page done.

--- scrapeQuote.py
+++ scrapeQuote.py
@@ -11,15 +11,15 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-             'https://www.russianfood.com/recipes/recipe.php?rid=124581',
-              'https://www.russianfood.com/recipes/recipe.php?rid=125466',
-              'https://www.russianfood.com/recipes/recipe.php?rid=116458',
-              'https://www.russianfood.com/recipes/recipe.php?rid=113251',
-             'https://www.russianfood.com/recipes/recipe.php?rid=157024',
-             'https://www.russianfood.com/recipes/recipe.php?rid=162162',
-             'https://www.russianfood.com/recipes/recipe.php?rid=120468',
-             'https://www.russianfood.com/recipes/recipe.php?rid=122523',
-             'https://www.russianfood.com/recipes/recipe.php?rid=121840'
+              'https://www.russianfood.com/recipes/recipe.php?rid=124581',
+              #'https://www.russianfood.com/recipes/recipe.php?rid=125466',
+              #'https://www.russianfood.com/recipes/recipe.php?rid=116458',
+              #'https://www.russianfood.com/recipes/recipe.php?rid=113251', Коврижка!!!!
+              #'https://www.russianfood.com/recipes/recipe.php?rid=157024',
+              'https://www.russianfood.com/recipes/recipe.php?rid=162162',
+              #'https://www.russianfood.com/recipes/recipe.php?rid=120468',
+              #'https://www.russianfood.com/recipes/recipe.php?rid=122523',
+              #'https://www.russianfood.com/recipes/recipe.php?rid=121840'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -55,21 +55,31 @@ class QuotesSpider(scrapy.Spider):
         else:
             result_time = 'Oooops!'
 
-
+        if len(result_time) == 0:
+            time_in_minutes = 'No time'
+        else:
+            x = int(result_time[0])
+            if x < 10:
+                if len(result_time) == 2:
+                    hours = int(result_time[0])
+                    minutes = int(result_time[1])
+                    time_in_minutes = hours*60 + minutes
+                else:
+                    hours = int(result_time[0])
+                    time_in_minutes = hours * 60
+            else:
+                time_in_minutes = result_time
 
         yield {
 
-            'result_time': result_time,
-            'Time': all_numbers,
-            #'TimesAll': response.xpath('//div[@class="sub_info"]//div[@class="el"]//b/text()').getall(),
-            # Таблиця продуктів з тегами
-            #'Products_table': response.xpath('//*[@class="ingr"]').get(),
-            #'Products_table_text': response.xpath('//*[@class="ingr"]//td//span/text()').getall(),
-
-            'Number of servings': response.xpath('//*[@class="portion"]/text()').re('\d+'),
             'ID': re.search(r'(?<==)\w+', id).group(0),
             'Name_recipe': response.xpath('//h1/text()').get(),
-            'Desctiption_recipe': response.xpath('//div[@class="rcp_share_block_top"]').css('div::attr(data-description)').get()
+            'Desctiption_recipe': response.xpath('//div[@class="rcp_share_block_top"]').css('div::attr(data-description)').get(),
+            'Time in minutes': time_in_minutes,
+            'Number of servings': response.xpath('//*[@class="portion"]/text()').re('\d+'),
+            'Products_table': response.xpath('//*[@class="ingr"]').get(),
+            'Products_table_text': response.xpath('//*[@class="ingr"]//td//span/text()').getall(),
+            'Cooking': response.xpath('//*[@class="step_n"]/p/text()').getall(),
         }
 
         # 'Print_version_LINK': response.xpath('//div[@class="print_ver padding_l"]//span/@href').get(),
